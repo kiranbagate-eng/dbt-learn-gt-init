eb91b2e62c702190c6c9702d395793ac4d39f50a
kiran_bagate <kiran_bagate@epam.com>
2026-04-12 08:43:56 +0530

    Add package codegen. Create source for stripe and add staging model payment.

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -1,8 +1,22 @@
-select 
-    id as payment_id,
-    orderid as order_id,
-    paymentmethod as payment_method,
-    amount/100 as amount,
-    status,
-    created as created_at
-from raw.stripe.payment
+with source as (
+
+    select * from {{ source('stripe', 'payment') }}
+
+),
+
+renamed as (
+
+    select
+        id as payment_id,
+        orderid as order_id,
+        paymentmethod as payment_method,
+        amount/100 as amount,
+        status,
+        created as created_at,
+        _batched_at
+
+    from source
+
+)
+
+select * from renamed
