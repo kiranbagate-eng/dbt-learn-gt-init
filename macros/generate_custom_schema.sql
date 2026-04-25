6757672806ee309a5faa9894f4c27746d842fd81
kiran_bagate <kiran_bagate@epam.com>
2026-04-25 08:48:35 +0530

    Added generate_csuton_schema macro

---
+++ macros/generate_custom_schema.sql
@@ -0,0 +1,15 @@
+{% macro generate_schema_name(custom_schema_name, node) -%}
+
+    {%- set default_schema = target.schema -%}
+    {%- set target_name = target.name -%}
+    {%- if custom_schema_name is none -%}
+
+        {{ default_schema }}
+
+    {%- else -%}
+
+        {{ default_schema }}_{{ custom_schema_name | trim }}
+
+    {%- endif -%}
+
+{%- endmacro %}
